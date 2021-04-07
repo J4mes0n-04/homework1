37871ad36a9c9d6fba6706c38c252182d3f5ad25
Maxim <maksgmq@mail.ru>
2021-04-07 19:57:56 +0500

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson1/FirstClass.java
@@ -0,0 +1,60 @@
+//
+// Created by Glazyrin Maksim
+//
+package ru.geekbrains.lesson1;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+    public static void printThreeWords() {
+        System.out.println("_Orange");
+        System.out.println("_Banana");
+        System.out.println("_Apple");
+    }
+
+    public static void checkSumSign() {
+        int a;
+        int b;
+        a = 25;
+        b = -75;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+                System.out.println("Сумма отрицательна");
+            }
+        }
+
+    public static void printColor() {
+        int value = 125;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 1 && value < 100) {
+            System.out.println("Жёлтный");
+        }
+        if (value >= 101) {
+            System.out.println("Зеленый");
+        }
+
+    }
+
+    public static void compareNumbers() {
+        int a;
+        int b;
+            a = 20;
+            b = 40;
+
+        if (a >= b)
+            System.out.println("a >= b");
+        if (a < b)
+            System.out.println("a < b");
+    }
+
+}
