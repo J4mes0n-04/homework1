f6ae7991ed95ea17a981ed8150451a500f9f3ec5
Maksim <77258464+J4mes0n-04@users.noreply.github.com>
2021-04-11 23:14:32 +0500

    Update FirstClass.java

--- src/main/java/ru/geekbrains/lesson1/FirstClass.java
+++ src/main/java/ru/geekbrains/lesson1/FirstClass.java
@@ -1,60 +1,44 @@
 //
-// Created by glazyrin Maksim
+// Created by Glazyrin Maksim
 //
-package ru.geekbrains.lesson1;
 
-public class FirstClass {
+package geekbrains_lesson2;
+
+public class Lesson2 {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-
-    }
-    public static void printThreeWords() {
-        System.out.println("_Orange");
-        System.out.println("_Banana");
-        System.out.println("_Apple");
+        System.out.println(within10and20(15, 15));
+        isPositiveOrNegative(0);
+        System.out.println(isNegative(0));
+        printWordNTimes("word",5);
     }
 
-    public static void checkSumSign() {
-        int a;
-        int b;
-        a = 25;
-        b = -75;
-
-        if (a + b >= 0) {
-            System.out.println("Сумма положительная");
+    public static boolean within10and20(int a, int b) {
+        int sum = a + b;
+        if ((sum <= 20) && (sum >= 10)) {
+            return true;
         } else {
-                System.out.println("Сумма отрицательна");
-            }
+            return false;
         }
-
-    public static void printColor() {
-        int value = 125;
-
-        if (value <= 0) {
-            System.out.println("Красный");
-        }
-        if (value > 1 && value < 100) {
-            System.out.println("Жёлтный");
-        }
-        if (value >= 101) {
-            System.out.println("Зеленый");
-        }
-
     }
 
-    public static void compareNumbers() {
-        int a;
-        int b;
-            a = 20;
-            b = 40;
-
-        if (a >= b)
-            System.out.println("a >= b");
-        if (a < b)
-            System.out.println("a < b");
+    public static void isPositiveOrNegative(int x) {
+        if (x >= 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
     }
 
+    public static boolean isNegative(int c) {
+        if (c >= 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public static void printWordNTimes(String word, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(word);
+        }
+    }
 }
